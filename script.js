63b741ffe8bc1a2062f4cdc2b6ba3595e12b43ee
lucianatux <lucianatux@yahoo.com.ar>
2024-06-20 19:26:28 -0300

    imagenes

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const briseida = document.getElementById('briseida');
+document.addEventListener("DOMContentLoaded", function () {
+  // BRISEIDA JS
+  const briseida = document.getElementById("briseida");
 
-    briseida.addEventListener('mouseover', function() {
-        briseida.style.transform = 'translateX(300px)';
+  briseida.addEventListener("mouseover", function () {
+    briseida.style.transform = "translateX(300px)";
+  });
+
+  briseida.addEventListener("transitionend", function () {
+    if (briseida.style.transform === "translateX(300px)") {
+      briseida.style.transition = "none";
+      briseida.style.transform = "translateX(0)";
+      briseida.style.opacity = "1";
+
+      briseida.offsetHeight;
+
+      briseida.style.transition = "transform 1s linear, opacity 0.5s linear";
+    }
+  });
+
+  // SEARCH JS
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-button");
+  const contentItems = document.querySelectorAll(".content-item");
+
+  searchButton.addEventListener("click", function () {
+    const query = searchInput.value.toLowerCase();
+    contentItems.forEach(function (item) {
+      if (item.textContent.toLowerCase().includes(query)) {
+        item.style.display = "";
+      } else {
+        item.style.display = "none";
+      }
     });
+  });
 
-    briseida.addEventListener('transitionend', function() {
-        if (briseida.style.transform === 'translateX(300px)') {
-            briseida.style.transition = 'none';
-            briseida.style.transform = 'translateX(0)';
-            briseida.style.opacity = '1';
+  searchInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      searchButton.click();
+    }
+  });
 
-            // Forzar un reflujo para que el cambio de estilo se aplique inmediatamente
-            briseida.offsetHeight;
 
-            briseida.style.transition = 'transform 1s linear, opacity 0.5s linear';
-        }
-    });
+
+  
 });
+
+

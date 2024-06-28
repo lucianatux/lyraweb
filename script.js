a7e06543d7f8a8df462b577d29aee744fcd1c31a
lucianatux <lucianatux@yahoo.com.ar>
2024-06-28 19:57:57 -0300

    carrusel responsive

--- script.js
+++ script.js
@@ -48,6 +48,59 @@ document.getElementById('formulario-contacto').addEventListener('submit', functi
 });
 
 
+// CARRUSEL RESPONSIVE
+
+if (window.innerWidth <= 767.98) {
+  var carouselInner = document.querySelector('#carouselFade1 .carousel-inner');
+  var slides = document.querySelectorAll('#carouselFade1 .carousel-item');
+
+  slides.forEach(function (slide) {
+    var secondImgAndText = slide.querySelector('.img-and-text.second');
+    if (secondImgAndText) {
+      // Crear una nueva slide
+      var newSlide = document.createElement('div');
+      newSlide.classList.add('carousel-item');
+      
+      // Crear un nuevo contenedor para la imagen y descripción
+      var newSlides = document.createElement('div');
+      newSlides.classList.add('slides', 'd-flex', 'w-100');
+
+      // Clonar el segundo conjunto de imagen y descripción
+      var clonedSecondImgAndText = secondImgAndText.cloneNode(true);
+      clonedSecondImgAndText.classList.remove('second');
+      clonedSecondImgAndText.classList.add('first');
+
+      // Agregar el conjunto clonado al nuevo contenedor
+      newSlides.appendChild(clonedSecondImgAndText);
+
+      // Agregar el nuevo contenedor a la nueva slide
+      newSlide.appendChild(newSlides);
+
+      // Agregar la nueva slide al carrusel
+      carouselInner.appendChild(newSlide);
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
